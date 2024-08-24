128327507cfccd31f1fb118c901b0bbdc94d3ffb
Adam Carr <156648324+realadamcarr@users.noreply.github.com>
2024-08-24 12:46:22 +0100

    refactor

--- StudentBinarySearchCliApp/StudentBinarySearchCliApp.cpp
+++ StudentBinarySearchCliApp/StudentBinarySearchCliApp.cpp
@@ -3,18 +3,63 @@
 
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+void insertionSort(string array[], int size) {
+    for (int index = 1; index < size; ++index) {
+        string key = array[index];
+        int j = index - 1;
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            j = j - 1;
+        }
+        array[j + 1] = key;
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int binarySearch(const string arr[], int left, int right, const string& target) {
+    while (left <= right) {
+        int midpoint = left + (right - left) / 2;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+        if (arr[midpoint] == target)
+            return midpoint;
+
+        if (arr[midpoint] < target)
+            left = midpoint + 1;
+        else
+            right = midpoint - 1;
+    }
+    return -1;
+}
+
+int main() {
+
+    string array[] = { "Jon", "Dot", "Eva", "Roy", "Guy", "Jan", "Tom", "Jim", "Ann", "Kim", "Ron", "Tim", "Kay", "Amy" };
+	int byteSize = sizeof(array);
+	int stringSize = sizeof(array[0]);
+	int size = byteSize / stringSize;
+
+
+    insertionSort(array, size);
+
+    cout << "Sorted array:\n";
+    for (int index = 0; index < size; ++index) {
+        std::cout << array[index] << " ";
+    }
+    std::cout << std::endl;
+
+
+    string value;
+    cout << "Enter a string to search: ";
+    cin >> value;
+
+    int valueIndex = binarySearch(array, 0, size - 1, value);
+    if (valueIndex != -1)
+        std::cout << "Position of '" << value << "' in the sorted array: " << valueIndex << std::endl;
+    else
+        std::cout << "String not found in the array." << std::endl;
+
+    return 0;
+}
+
